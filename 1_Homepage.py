e10db4a931b695e0e8748df73b8c6042b122de13
akhfadhil <77158166+akhfadhil@users.noreply.github.com>
2024-05-29 18:00:17 +0700

    Add files via upload

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,33 +1,30 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "Heart Attack Prediction",
+    page_title = "Heart Disease Prediction",
     page_icon="🧊",
     layout = "wide"
 )
 
-st.title('Heart Disease Prediction')
-url = "https://www.linkedin.com/in/akhmad-fadhil/"
-
-i, ii, iii = st.columns(3)
+i, ii, iii = st.columns([1,2,1])
 with ii:
-    st.image('heart.jpg', caption='Healthy heart', width=500)
+    st.markdown("<h1 style='text-align: center; color: white;'>Heart Disease Prediction</h1>", unsafe_allow_html=True)
+    st.image('heart.jpg', caption='Healthy heart', width=700)
 
-# with st.container(border=True):
-    # st.markdown(
-    #     """
-    #     Penyakit jantung adalah penyebab utama kematian bagi masyarakat di Amerika. Penyakit tersebut diderita hampir oleh seluruh ras yang ada di Amerika seperti Afrika-Amerika, 
-    #     Indian Amerika, dan penduduk asli Alaska, serta kulit putih. Studi terbaru dari Centers of Disease Control and Prevention (CDC) menunjukkan bahwa hamper separuh penduduk 
-    #     Amerika (47%) memiliki setidaknya 1 dari 3 faktor resiko utama penyakit jantung. Faktor resiko utama tersebut antara lain:
-    #     - Tekanan darah tinggi
-    #     - Kolesterol tinggi
-    #     - Merokok
-    #     """)
-    # st.markdown(
-    #     """
-    #     Selain itu, indikator lain yang yang dapat menyebabkan penyakit jantung antara lain status diabetes, obesitas (BMI tinggi), kurang aktivitas fisik, atau terlalu banyak 
-    #     minum alkohol. 
-    #     \nOleh karena itu, mengidentifikasi dan mencegah faktor yang memiliki dampak terbesar terhadap penyakit jantung sangat penting untuk meminimalisir kematian. 
-    #     Dalam konteks ini, faktor yang akan dianalisis yaitu perilaku kesehatan untuk mencari insight yang dapat dilitindaklanjuti sehingga mengurangi penderita penyakit jantung. 
-    #     """)
-        
+with st.container(border=True):
+    st.markdown(
+        """
+        \nMenurut Kementerian Kesehatan (Kemenkes), penyakit jantung menjadi penyebab kematian terbanyak di Indonesia. Tidak hanya di Indonesia, kedua penyakit kardiovaskular tersebut 
+        juga menjadi perhatian dunia. Sebagian besar negara di dunia mengalokasikan sebagian besar anggarannya untuk memastikan warganya sehat. Namun, negara-negara masih belum mampu 
+        memenuhi permintaan layanan medis yang ideal bagi warganya karena kurangnya keahlian medis di berbagai rumah sakit. 
+        \nSistem diagnosis medis telah banyak diterapkan untuk mendiagnosis gejala penyakit seperti kanker dan diabetes. Namun, alat dan metode analisis 
+        tidak cukup untuk mengidentifikasi hubungan tersembunyi dalam gejala penyakit jantung. Akibatnya, rasio penderita penyakit ini meningkat pesat hingga 
+        12 juta kematian setiap tahun. Saling ketergantungan yang kompleks pada berbagai gejala penyakit ini menunjukkan sulitnya 
+        mendiagnosis penyakit jantung pada tahap awal. Dokter tidak memiliki cukup waktu untuk menangani setiap kasus dan mengalami kesulitan dalam mengikuti perkembangan 
+        aplikasi terbaru. Banyak metode alternatif telah disarankan untuk diagnosis medis di bidang kesehatan. Namun, mengevaluasi fungsionalitas sistem diagnosis penyakit jantung
+        masih merupakan tantangan. 
+        \nSalah satu metode yang dapat digunakan yaitu machine learning. Dengan menggunakan sistem dan alat yang dirancang untuk mengurutkan dan mengkategorikan data, algoritma 
+        machine learning dapat menemukan pola dalam kumpulan data yang memungkinkan para profesional di bidang medis mengidentifikasi penyakit jantung dan melakukan pencegahan.
+        Data yang digunakan pada sistem ini berasal dari repositori machine learning Universitas California Irvine. Algoritma machine learning yang digunakan pada sistem ini yaitu 
+        Logistic Regression yang memiliki akurasi sebesar 90%. 
+        """)
